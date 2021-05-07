b6d9cc30e4516e673906f939b50b06636eea9ae7
vhaovvvx <vhaovvvx@gmail.com>
2021-05-07 22:44:28 +0700

    new commit

--- js/app2.js
+++ js/app2.js
@@ -1,18 +1,21 @@
 const carouselSlide = document.querySelector(".trailer-siema__slider");
 // console.log(carouselSlide);
 const carouselVideos = document.querySelectorAll(".slider-videos video");
+console.log(carouselVideos[0]);
 const videos = document.getElementsByTagName("video");
 //buttons
 const prevBtn = document.querySelector("#trailer-prev__btn");
 const nextBtn = document.querySelector("#trailer-next__btn");
 //counter
 let counter = 1;
-const changImg = 13.4;
+const carouselSlide2 = document.querySelector(".trailer-container");
+
+const widthhh = carouselSlide2.clientWidth / 2;
 //slidershow
 function next() {
   if (counter >= carouselVideos.length - 1) return;
   carouselSlide.style.transition = "transform 0.4s ease-in-out";
-  carouselSlide.style.transform = "translateX(" + -changImg * counter + "%)";
+  carouselSlide.style.transform = "translateX(" + -widthhh * counter + "px)";
   counter++;
 }
 
@@ -20,7 +23,7 @@ function prev() {
   if (counter <= 0) return;
   carouselSlide.style.transition = "transform 0.4s ease-in-out";
   counter--;
-  carouselSlide.style.transform = "translateX(" + -changImg * counter + "%)";
+  carouselSlide.style.transform = "translateX(" + -widthhh * counter + "px)";
 }
 
 nextBtn.addEventListener("click", () => {
@@ -38,14 +41,14 @@ carouselSlide.addEventListener("transitionend", () => {
   if (carouselVideos[counter].id === "lastClone") {
     carouselSlide.style.transition = "none";
     counter = carouselVideos.length - 2;
-    carouselSlide.style.transform = "translateX(" + -changImg * counter + "%)";
+    carouselSlide.style.transform = "translateX(" + -widthhh * counter + "px)";
   }
 });
 carouselSlide.addEventListener("transitionend", () => {
   if (carouselVideos[counter].id === "firstClone") {
     carouselSlide.style.transition = "none";
     counter = carouselVideos.length - counter;
-    carouselSlide.style.transform = "translateX(" + -changImg * counter + "%)";
+    carouselSlide.style.transform = "translateX(" + -widthhh * counter + "px)";
   }
 });
 
@@ -211,21 +214,90 @@ const nav = document.getElementById("nav");
 const hiddenNavClick = document.getElementsByClassName(
   "country-navigation-wrapper"
 );
+const hiddenNavBuy = document.getElementsByClassName("nav-buy");
 
 function scrollFunction() {
   if (document.documentElement.scrollTop > 700) {
     nav.style.position = "fixed";
     nav.style.width = "100%";
-    nav.style.zIndex = "9999";
+    nav.style.zIndex = "999";
     nav.style.top = "0";
     nav.style.backgroundColor = "black";
     hiddenNavClick[0].style.display = "none";
+    hiddenNavBuy[0].style.visibility = "initial";
   } else {
     nav.style.top = "0px";
     nav.style.position = "initial";
     nav.style.width = "100%";
-    nav.style.zIndex = "9999";
+    nav.style.zIndex = "999";
     nav.style.backgroundColor = "transparent";
     hiddenNavClick[0].style.display = "block";
+    hiddenNavBuy[0].style.visibility = "hidden";
   }
 }
+
+const overlay = document.getElementsByClassName("overlay");
+const lauchEditionClickOverlay = document.getElementsByClassName(
+  "click-buy__launch-edition"
+);
+const elementClickOverlay = [
+  ...document.getElementsByClassName("buynowButton"),
+];
+console.log(elementClickOverlay);
+const steelbookEditionClickOverlay = document.getElementsByClassName(
+  "click-buy__steelbook-edition"
+);
+const giftWithPurchaseClickOverlay = document.getElementsByClassName(
+  "click-buy__gift-with-purchase"
+);
+const buttonNewsLetter = document.getElementsByClassName(
+  "click-buy__newsletter"
+);
+
+elementClickOverlay.forEach((Elmt) => {
+  Elmt.addEventListener("click", function (e) {
+    e.preventDefault();
+    let buyButtonLaunchEdition = e.target.id == "launch-edition-buy";
+    let buyButtonSteelBookEdition = e.target.id == "steelbook-edition-buy";
+    let buyButtonGiftWithPurchase = e.target.id == "gift-with-purchase";
+    let buyButtonNewsLetter = e.target.id == "newsletter";
+    let subscribeToTheNewsLetter = e.target.id == "newsletter2";
+    console.log(subscribeToTheNewsLetter);
+
+    if (buyButtonLaunchEdition) {
+      overlay[0].style.display = "block";
+      lauchEditionClickOverlay[0].style.display = "block";
+    } else if (buyButtonSteelBookEdition) {
+      overlay[0].style.display = "block";
+      steelbookEditionClickOverlay[0].style.display = "block";
+    } else if (buyButtonGiftWithPurchase) {
+      overlay[0].style.display = "block";
+      giftWithPurchaseClickOverlay[0].style.display = "block";
+    } else if (buyButtonNewsLetter || subscribeToTheNewsLetter) {
+      overlay[0].style.display = "block";
+      buttonNewsLetter[0].style.display = "block";
+    }
+  });
+});
+const close = [...document.getElementsByClassName("close")];
+close.forEach((closeElm) => {
+  closeElm.addEventListener("click", function (e) {
+    e.preventDefault();
+    const closeBtn = e.target.className == "close";
+    console.log(closeBtn);
+    if (closeBtn) {
+      overlay[0].style.display = "none";
+      lauchEditionClickOverlay[0].style.display = "none";
+      steelbookEditionClickOverlay[0].style.display = "none";
+      giftWithPurchaseClickOverlay[0].style.display = "none";
+      buttonNewsLetter[0].style.display = "none";
+    }
+  });
+});
+overlay[0].addEventListener("click", function () {
+  overlay[0].style.display = "none";
+  lauchEditionClickOverlay[0].style.display = "none";
+  steelbookEditionClickOverlay[0].style.display = "none";
+  giftWithPurchaseClickOverlay[0].style.display = "none";
+  buttonNewsLetter[0].style.display = "none";
+});
---
+++ js/green.js
@@ -0,0 +1,32 @@
+// Detect if a link's href goes to the current page
+function getSamePageAnchor(link) {
+  if (
+    link.protocol !== window.location.protocol ||
+    link.host !== window.location.host ||
+    link.pathname !== window.location.pathname ||
+    link.search !== window.location.search
+  ) {
+    return false;
+  }
+
+  return link.hash;
+}
+
+// Scroll to a given hash, preventing the event given if there is one
+function scrollToHash(hash, e) {
+  const elem = hash ? document.querySelector(hash) : false;
+  if (elem) {
+    if (e) e.preventDefault();
+    gsap.to(window, { scrollTo: elem });
+  }
+}
+
+// If a link's href is within the current page, scroll to it instead
+document.querySelectorAll("a[href]").forEach((a) => {
+  a.addEventListener("click", (e) => {
+    scrollToHash(getSamePageAnchor(a), e);
+  });
+});
+
+// Scroll to the element in the URL's hash on load
+scrollToHash(window.location.hash);
